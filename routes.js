ab1230b4f7987bf254d55241c6e9895e4321f1de
sourabh696969 <sourabhkumar2462@gmail.com>
2025-03-07 18:24:53 +0530

    update

--- routes.js
+++ routes.js
@@ -7,8 +7,5 @@ router.use("/user", require("./routes/userRoute"));
 router.use("/tag", require("./routes/tagRoute"));
 router.use("/site", require("./routes/siteRoute"));
 
-<<<<<<< HEAD
 module.exports = router;
-=======
-module.exports = router;
->>>>>>> 30826dad66e13e4335c53bbebd79a5dcfe5d574c
+
