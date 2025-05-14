0d544f5d6ff60eb1a42e8cc97beedff40759ad68
techazora <techazora@gmail.com>
2025-05-14 23:02:10 +0530

    update

--- routes/mqttLightRoute.js
+++ routes/mqttLightRoute.js
@@ -1,10 +1,22 @@
 const express = require("express");
-const { handleSingleLight, handleLight, handleLightsByTagId } = require("../controllers/mqttLightController");
+const {
+  handleSingleLight,
+  handleLight,
+  handleLightsByTagId,
+  handleRandomLightsByPoles,
+  handleChaseLightsByPoles,
+  handleMarqueeLightsByPoles,
+  handleWaveLightsByPoles,
+} = require("../controllers/mqttLightController");
 
 const router = express.Router();
 
 router.patch("/handleSingleLight/:lightId", handleSingleLight);
 router.patch("/handleLightsByTagId/:tagId", handleLightsByTagId);
+router.patch("/handleRandomLightsByPoles", handleRandomLightsByPoles);
+router.patch("/handleChaseLightsByPoles", handleChaseLightsByPoles);
+router.patch("/handleMarqueeLightsByPoles", handleMarqueeLightsByPoles);
+router.patch("/handleWaveLightsByPoles", handleWaveLightsByPoles);
 router.patch("/handleLight", handleLight);
 
 module.exports = router;
